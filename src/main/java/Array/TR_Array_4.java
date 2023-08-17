3b0e0525588a3abd55bd3d99e5c9935429f297b9
ekimaycicek <ekimaycicek@icloud.com>
2023-08-17 20:23:02 +0300

    new

--- src/main/java/Array/TR_Array_4.java
+++ src/main/java/Array/TR_Array_4.java
@@ -4,5 +4,20 @@ public class TR_Array_4 {
 
     /*
 
+(TR)    Aşağıdaki grid şeklini yazan Java Kodunu yazınız.
+
+             Beklenen Çıktı:
+
+             0  0  0  0  0  0  0  0  0  0
+             0  0  0  0  0  0  0  0  0  0
+             0  0  0  0  0  0  0  0  0  0
+             0  0  0  0  0  0  0  0  0  0
+             0  0  0  0  0  0  0  0  0  0
+             0  0  0  0  0  0  0  0  0  0
+             0  0  0  0  0  0  0  0  0  0
+             0  0  0  0  0  0  0  0  0  0
+             0  0  0  0  0  0  0  0  0  0
+             0  0  0  0  0  0  0  0  0  0
+
      */
 }
